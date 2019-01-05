3c79b64b471f3ae60b70a88c59855fead02aad6d
Andrew Nadolski <andrew.nadolski@gmail.com>
2019-01-05 16:35:27 -0600

    Add more tests for core.py; flag expected failures
    Tests now exist for all but one function (core.rt_amp). All tests that
    check behavior at an angle of incidence of pi/2 have been decorated as
    expected failures. The core.py functions that handle angles need a close
    check to make sure they're doing the right thing at different angles.
    
    Angle-dependent results will have to wait until the normal incidence
    case is completely handled, however.

--- test/test_core.py
+++ test/test_core.py
@@ -7,14 +7,6 @@ import numpy as np
 from armm import core
 
 class CoreTest(unittest.TestCase):
-    def setUp(self):
-        # We need to create a set of default values that we can use as
-        # a baseline.
-        self.index = 1.
-        self.thick = 1.
-        self.tand = 0.
-        self.freq = 150e9 # frequency in Hz
-
     def test_rt_amp(self):
         pass
 
@@ -25,7 +17,7 @@ class CoreTest(unittest.TestCase):
         for v in vals:
             with self.subTest(i=v):
                 self.assertEqual(core.r_power(v[0]), v[1],
-                    'Unexpected reflection power result.')
+                    msg='Unexpected reflection power result.')
 
     def test_t_power_normal_incidence(self):
         amps = [-2., 0., 2.]
@@ -34,17 +26,8 @@ class CoreTest(unittest.TestCase):
         for v in vals:
             with self.subTest(i=v):
                 self.assertEqual(core.t_power(v[0], 1., 2., 0., 0.), v[1],
-                    'Unexpected transmission power result.')
+                    msg='Unexpected transmission power result.')
 
-#    def test_t_power_normal_orthogonal(self):
-#        amps = [-2., 0., 2.]
-#        for a in amps:
-#            with self.subTest(i=a):
-#                self.assertAlmostEqual(
-#                    core.t_power(a, 1., 2., np.pi/2., np.pi/2.), 0.,
-#                    places=18,
-#                    msg='Unexpected transmission power result.')
-    
     def test_r_interface_normal_incidence(self):
         params = [(0., 's'), (0., 'p')]
         ans = [-1./3., 1./3.]
@@ -57,33 +40,93 @@ class CoreTest(unittest.TestCase):
                         ' in the vicinity of pi/2 are not currently handled well'
                         ' and result in strange behavior.')
 
-#    def test_r_interface_orthogonal_incidence(self):
-#        params = [(np.pi/2., 's'), (np.pi/2, 'p')]
-#        ans = [0, 0]
-#        vals = zip(params, ans)
-#        for v in vals:
-#            with self.subTest(i=v):
-#                self.assertEqual(
-#                    core.r_interface(1., 2., v[0][0], v[0][0], v[0][1]), v[1],
-#                    msg='Unexpected reflection at interface. Note: Trig operations'
-#                        ' in the vicinity of pi/2 are not currently handled well'
-#                        ' and result in strange behavior.')
+    def test_t_interface_normal_incidence(self):
+        params = [(0., 's'), (0., 'p')]
+        ans = [2./3., 2./3.]
+        vals = zip(params, ans)
+        for v in vals:
+            with self.subTest(i=v):
+                self.assertEqual(
+                    core.t_interface(1., 2., v[0][0], v[0][0], v[0][1]), v[1],
+                    msg='Unexpected transmission at interface. Note: Trig operations'
+                        ' in the vicinity of pi/2 are not currently handled well'
+                        ' and result in strange behavior.')
 
+    def test_wavenumber_normal_incidence(self):
+        lossy = [True, False]
+        ans = [0., 0.]
+        vals = zip(lossy, ans)
+        for v in vals:
+            with self.subTest(i=v):
+                self.assertEqual(
+                    core.wavenumber(150e9, 2., 0.001, 0., v[0]), v[1],
+                    msg='Unexpected transmission at interface. Note: Trig operations'
+                        ' in the vicinity of pi/2 are not currently handled well'
+                        ' and result in strange behavior.')
 
-    def test_t_interface(self):
-        pass
+    @unittest.expectedFailure
+    def test_t_power_orthogonal_incidence(self):
+        amps = [-2., 0., 2.]
+        for a in amps:
+            with self.subTest(i=a):
+                self.assertAlmostEqual(
+                    core.t_power(a, 1., 2., np.pi/2., np.pi/2.), 0.,
+                    places=18,
+                    msg='Unexpected transmission power result. Note: Trig operations'
+                        ' in the vicinity of pi/2 are not currently handled well'
+                        ' and result in strange behavior.')
 
-    def test_wavenumber(self):
-        pass
+    @unittest.expectedFailure
+    def test_r_interface_orthogonal_incidence(self):
+        params = [(np.pi/2., 's'), (np.pi/2, 'p')]
+        ans = [0, 0]
+        vals = zip(params, ans)
+        for v in vals:
+            with self.subTest(i=v):
+                self.assertEqual(
+                    core.r_interface(1., 2., v[0][0], v[0][0], v[0][1]), v[1],
+                    msg='Unexpected reflection at interface. Note: Trig operations'
+                        ' in the vicinity of pi/2 are not currently handled well'
+                        ' and result in strange behavior.')
+
+    @unittest.expectedFailure
+    def test_t_interface_orthogonal_incidence(self):
+        params = [(np.pi/2., 's'), (np.pi/2., 'p')]
+        ans = [0., 0.]
+        vals = zip(params, ans)
+        for v in vals:
+            with self.subTest(i=v):
+                self.assertEqual(
+                    core.t_interface(1., 2., v[0][0], v[0][0], v[0][1]), v[1],
+                    msg='Unexpected transmission at interface. Note: Trig operations'
+                        ' in the vicinity of pi/2 are not currently handled well'
+                        ' and result in strange behavior.')
+
+    @unittest.expectedFailure
+    def test_wavenumber_orthogonal_incidence(self):
+        lossy = [True, False]
+        ans = [0., 0.]
+        vals = zip(lossy, ans)
+        for v in vals:
+            with self.subTest(i=v):
+                self.assertEqual(
+                    core.wavenumber(150e9, 2., 0.001, np.pi/2., v[0]), v[1],
+                    msg='Unexpected transmission at interface. Note: Trig operations'
+                        ' in the vicinity of pi/2 are not currently handled well'
+                        ' and result in strange behavior.')
+
+    def test_prop_wavenumber(self):
+        ks = np.array([1., 1., 2., 2.])
+        ds = np.array([np.inf, 2., 2., np.inf])
+        ans = np.array([np.inf, 2., 4., np.inf])
+        self.assertTrue(np.array_equal(core.prop_wavenumber(ks, ds), ans),
+            msg='Unexpected propagation result')
 
     def test_make_2x2(self):
         nparr = np.array([[1., 2.], [3., 4.]])
         corearr = core.make_2x2(1., 2., 3., 4.)
-        self.assertTrue(np.array_equal(nparr.all(), corearr.all()),
-            'The output of core.make_2x2() is not the same as np.array()!')
-
-    def test_prop_wavenumber(self):
-        pass
+        self.assertTrue(np.array_equal(nparr, corearr),
+            msg='The output of core.make_2x2() is not the same as np.array()!')
 
 if __name__ == '__main__':
     unittest.main()
