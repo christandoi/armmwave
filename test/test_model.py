fd4e9ea4e5bc5792d380cd701d68587273edc4cc
Andrew Nadolski <andrew.nadolski@gmail.com>
2019-01-08 12:11:31 -0600

    Add set_up() tests for error checks/index matching
    Created two new tests for model.Model().set_up. The first checks for the
    exceptions that we catch when the function is called. The second checks
    that the index-matching option that links the Terminator layer and the
    layer before it works properly.

--- test/test_model.py
+++ test/test_model.py
@@ -5,6 +5,7 @@ Contains tests for the `Model` class
 import unittest
 import numpy as np
 from armm import model
+from armm import layer
 
 class ModelTest(unittest.TestCase):
     def setUp(self):
@@ -26,3 +27,32 @@ class ModelTest(unittest.TestCase):
         single_modarr = self.model.set_freq_range(low, hi)
         self.assertTrue(np.array_equal(single_chkarr, single_modarr),
             msg='Frequency arrays are not equal.')
+
+    def test_set_up_errors(self):
+        # layers1 should throw an IndexError; layers2 should throw a
+        # TypeError; layers3 should throw a TypeError
+        layers1 = [[layer.BaseLayer()]]
+        layers2 = [[layer.BaseLayer(), layer.BaseLayer(), layer.Terminator()]]
+        layers3 = [[layer.Source(), layer.BaseLayer(), layer.BaseLayer()]]
+        vals = layers1 + layers2 + layers3
+        errs = (IndexError, TypeError)
+        for v in vals:
+            with self.subTest(i=v):
+                self.assertRaises(errs, self.model.set_up, v[0])
+
+    def test_set_up_match_term_rind(self):
+        last_rind = -99.0
+        layers = [layer.Source(), layer.BaseLayer(),
+                  layer.BaseLayer(rind=last_rind), layer.Terminator()]
+        vac = [False, True]
+        ans = [last_rind, 1.]
+        vals = zip(vac, ans)
+        for v in vals:
+            with self.subTest(i=v):
+                layers[-1] = layer.Terminator(vac=v[0])
+                self.model.set_up(layers)
+#                print(self.model.struct)
+                terminator = self.model.struct[-1]
+                self.assertEqual(terminator.rind, v[1],
+                    msg='Unexpected Terminator refractive index. '
+                        'Got: {}'.format(terminator.rind))
