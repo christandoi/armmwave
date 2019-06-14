ad4a9c08de1863aaf2b08fe5af802de3d43b276a
Andrew Nadolski <andrew.nadolski@gmail.com>
2019-06-14 11:58:24 -0500

    Write tests for the layer module
    I've included tests for the layer module. These tests check that classes
    and subclasses are instantiated correctly, with the defaults we expect.
    All tests are passing at this time.

--- test/test_layer.py
+++ test/test_layer.py
@@ -7,50 +7,51 @@ import pytest
 import numpy as np
 from armm import layer
 
-def test_make_BaseLayer():
-    pass
+@pytest.mark.parametrize('test_l, expected', [
+    (layer.BaseLayer(), ['Basic layer', 1., 0., 1.]),
+    (layer.Layer(), ['Basic layer', None, None, 1., 0., 1.]),
+    (layer.Source(), ['Source layer', layer._Void(), 1., 0., np.inf]),
+    (layer.Terminator(), ['Terminator layer', layer._Void(), 1., 0., np.inf, True]),
+    ])
+def test_default_class_attrs(test_l, expected):
+    """
+    Check that classes and subclasses are properly instantiated
+    with sane default values.
+    """
+    test_attr = [test_l.__dict__[key] for key in sorted(test_l.__dict__.keys())]
+    assert test_attr == expected 
 
-def test_repr_BaseLayer():
-    pass
+def test_get_rind():
+    """Check that we get the refractive index we expect."""
+    test_l = layer.BaseLayer()
+    assert test_l.get_rind() == 1.
 
-def test_default_attr_BaseLayer():
-    pass
+def test_get_thick():
+    """Check that we get the thickness we expect."""
+    test_l = layer.BaseLayer()
+    assert test_l.get_thick() == 1.
 
-def test_make_Layer():
-    pass
+def test_get_tand():
+    """Check that we get the loss tangent we expect."""
+    test_l = layer.BaseLayer()
+    assert test_l.get_tand() == 0.
 
-def test_repr_Layer():
-    pass
+def test_get_desc():
+    """Check that we get the description we expect."""
+    test_l = layer.BaseLayer()
+    assert test_l.get_desc() == 'Basic layer'
 
-def test_make_Source():
-    pass
+@pytest.mark.parametrize('test_l, expected', [
+    (layer.BaseLayer(), 'Basic layer (Basic layer)'),
+    (layer.Layer(), 'Basic layer (Sim layer)'),
+    (layer.Source(), 'Source layer (Source layer)'),
+    (layer.Terminator(), 'Terminator layer (Terminator layer)'),
+    (layer._Void(), 'There is nothing but THE VOID'),
+    ])
+def test_default_repr(test_l, expected):
+    """Check that we get the string representation we expect."""
+    assert repr(test_l) == expected
 
-def test_repr_Source():
-    pass
-
-def test_Source_previous():
-    pass
-
-def test_default_attr_Source():
-    pass
-
-def test_make_Terminator():
-    pass
-
-def test_repr_Terminator():
-    pass
-
-def test_Terminator_next():
-    pass
-
-def test_default_attr_Terminator():
-    pass
-
-def test_make_Void():
-    pass
-
-def test_repr_Void():
-    pass
-
-def test_default_attr_Void():
-    pass
+def test_not_a_void():
+    """Check that we don't mistake a regular layer for The Void."""
+    assert not (layer.Source() == layer._Void())
