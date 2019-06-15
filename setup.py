e56c09407741123ea357b816734b95624d2103c9
Andrew Nadolski <andrew.nadolski@gmail.com>
2019-06-15 12:02:59 -0500

    Set python requirement back to >=3.5; bump version
    Updated the `.travis.yml` and `setup.py` to require python >=3.5 because
    we start running into dependency issues with 3.4 and below. I'll try to
    sort these out, but will go with this requirement for now.
    
    I've bumped the version number to 0.0a2 in preparation for a minor pip
    release.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name='armmwave',
-    version='0.0a1',
+    version='0.0a2',
     author='Andrew Nadolski',
     author_email='andrew.nadolski@gmail.com',
     description='Calculate mm-wave transmittance and reflectance of materials',
@@ -19,7 +19,7 @@ setup(
     platforms=['Linux', 'MacOS X'],
     url='https://github.com/anadolski/armmwave',
     install_requires=install_reqs,
-    python_requires='>=3.4',
+    python_requires='>=3.5',
     package_dir={'armmwave': 'armmwave'},
     packages=['armmwave'],
     tests_require=['pytest'],
